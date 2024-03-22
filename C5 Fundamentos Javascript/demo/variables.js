fb1212d5abe9739269666504f06bfe6653bf822d
LucianoTulian <lucianotulian4@gmail.com>
2024-03-21 23:08:37 -0300

    1.08hs

---
+++ C5 Fundamentos Javascript/demo/variables.js
@@ -0,0 +1,47 @@
+// var nombre1 = "tulian1"
+// var nombre2 = "tulian2"
+// var numero1 = 22220973
+// var numero2 = "22220973"
+// var numerodecimal = 22.220973
+// var numeronegativo = -22.220973
+
+// var booleano = true 
+// var booleano2 = false
+
+// var nulo = null
+// var indefinido = undefined
+
+
+// console.log(nombre1)
+// console.log(numerodecimal)
+// console.log(numero2,booleano,nulo,booleano2,nombre1)
+// console.log(noexista)
+
+// console.log(2132332)
+// console.log("string que puede ser una frace completa")
+// console.log(false)
+
+// console.log(frase, indefinido)
+// var frase = "el valor de indefinido"
+
+// print(nulo)
+
+// console.log(5.156456 * 3.1512564)
+// console.log(9 / 3)
+// console.log(7 - 19)
+// console.log(20 % 5)
+// console.log(20 / 5)
+
+// console.log(" operacion " + " de " + " concatenacion " + " de strings")
+// console.log("operacion " + 1234)
+
+// console.log("5" + 3)
+// console.log(3 + "8")
+// console.log("7" + "7")
+
+// console.log("5" + 3 + 8)
+// console.log(5 + 8 + "2")
+
+// console.log("5" == 5)
+// console.log("asdsd" == 5)
+console.log("5" === 5)
